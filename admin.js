472eced93eeac2834fa1057d646b8e1612550821
Yuta <yutaatamura@gmail.com>
2018-04-10 19:53:39 -0500

    Additions to admin.js

--- admin.js
+++ admin.js
@@ -22,7 +22,24 @@ function authorize() {
 
 function Admin() {
     
+    this.newUserSearch = function() {
+        //accept a name and location
+        //search for weather in area 
+        //save user's information in log
 
+        var newUser = new User("bob", "Houston");
+        newUser.getWeather();
+        fs.writeFile("log.txt", `
+User: ${newUser.name}
+Location: ${newUser.location}`, function(error, data) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Log.txt was updated!");
+        })
+
+    }
     //newusersearch accept a name and location 
     //getdata log or return list of all searches user have executed thus far 
     //
